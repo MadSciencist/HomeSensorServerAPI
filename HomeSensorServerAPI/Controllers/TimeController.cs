fcec380de881768b06b4d855beac7b5c4933a662
Mateusz Kryszczak <mkrysz1337@gmail.com>
2018-06-23 17:01:57 +0200

    Added time controller

---
+++ HomeSensorServerAPI/Controllers/TimeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HomeSensorServerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [Route("now")]
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetTime()
+        {
+            return Ok(DateTime.UtcNow.ToString());
+        }
+    }
+}
