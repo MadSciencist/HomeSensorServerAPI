6fef4411f00d43837a8e71ede151872464852947
Mateusz Kryszczak <mkrysz1337@gmail.com>
2018-08-01 21:24:58 +0200

    started moving frontend to separate folders and preparing build tool

---
+++ HomeSensorServerAPI/_client/src/js/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("smartHomeApp", ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "index.html",
+            controller: "RootController"
+        })
+        .otherwise({
+            templateUrl: "pages/not-found.html"
+        });
+});
+
+app.controller("RootController", function ($scope) {
+
+    $scope.test = 'TEST TEST TESTa ';
+
+});
