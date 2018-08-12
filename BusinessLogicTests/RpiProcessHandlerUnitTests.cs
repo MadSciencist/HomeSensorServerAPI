2a839d11431343aab3f34006c67946a9be831145
Mateusz Kryszczak <mkrysz1337@gmail.com>
2018-08-12 22:48:45 +0200

    started tests

--- BusinessLogicTests/RpiProcessHandlerUnitTests.cs
+++ BusinessLogicTests/RpiProcessHandlerUnitTests.cs
@@ -15,5 +15,14 @@ namespace BusinessLogicTests
 
             Assert.AreEqual(expectedResult, result);
         }
+
+        [Test]
+        public void ResolutionDictionaryReturnsInvalidValue()
+        {
+            var expectedResult = "640x480";
+            var result = Resolution.GetResolution(EResolution.Res720x480);
+
+            Assert.AreNotEqual(expectedResult, result);
+        }
     }
 }
