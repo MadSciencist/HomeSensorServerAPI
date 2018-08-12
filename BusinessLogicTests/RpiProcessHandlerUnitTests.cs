146a6cbc338dce89e0f79a16bd574eb5e8dbe982
Mateusz Kryszczak <mkrysz1337@gmail.com>
2018-08-12 22:40:51 +0200

    added test project

---
+++ BusinessLogicTests/RpiProcessHandlerUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RpiProcessHandler.FFmpeg;
+using System;
+
+namespace BusinessLogicTests
+{
+    public class RpiProcessHandlerUnitTests
+    {
+
+        [Test]
+        public void ResolutionDictionaryReturnsProperValue()
+        {
+            var expectedResult = "640x480";
+            var result = Resolution.GetResolution(EResolution.Res640x480);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
